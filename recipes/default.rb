06070913822355d5fcbd91efdfed25bf9f1f0c7c
Marek Hulan <mhulan@redhat.com>
2016-01-26 13:37:38 +0100

    Add packages for rpm platforms

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,19 @@
-%w(libvirt-bin libvirt-dev).each do |name|
-  package name do
-    action :nothing
-  end.run_action(:install)
-end
+case node[:platform]
+  when 'debian', 'ubuntu'
+    %w(libvirt-bin libvirt-dev).each do |name|
+      package name do
+        action :nothing
+      end.run_action(:install)
+    end
 
+  when 'redhat', 'centos', 'fedora'
+    %w(qemu-kvm libvirt virt-manager bridge-utils libvirt-devel).each do |name|
+      package name do
+        action :nothing
+      end.run_action(:install)
+    end
+end
+  
 %w(ruby-libvirt uuidtools).each do |name|
   chef_gem name do
     action :install
